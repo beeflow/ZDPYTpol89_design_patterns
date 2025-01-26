5c40d0112ea27e16fe08cff5a2fe4233fdd73654
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-01-26 13:40:50 +0000

    Insurance

---
+++ SQL/insurance.sql
@@ -0,0 +1,8 @@
+create database if not exists insurance;
+
+-- 1. ubezpieczyciel Aviva
+-- 2. ogólny rodzaj ubezpieczenia (na życie, OC, AC, dom, sprzęt, NNW, ...) + data od
+-- 3. zakres ubezpieczenia (warunki) - mogę użyć 3 razy w roku
+-- 4. kiedy skorzystałem z wybranego ubezpieczenia - chcę wiedzieć, czy np. mogę zrobić jakieś badanie, czy
+--        wykorzystałem już dostępną pulę badań.
+
