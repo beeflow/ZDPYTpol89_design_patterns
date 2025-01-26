3ecab50c456cf0e8404a2650df02acca208feabc
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-01-26 14:46:06 +0000

    Insurance - zadanie domowe

--- SQL/insurance.sql
+++ SQL/insurance.sql
@@ -7,18 +7,21 @@ create database if not exists insurance;
 --      4. kiedy skorzystałem z wybranego ubezpieczenia - chcę wiedzieć, czy np. mogę zrobić jakieś badanie, czy
 --          wykorzystałem już dostępną pulę badań.
 
+-- aviva
 create table insurer
 (
     id   int auto_increment primary key,
     name varchar(50) not null unique
 );
 
+-- np. dom
 create table insurance_type
 (
     id   int auto_increment primary key,
     name varchar(50) not null unique
 );
 
+-- aviva + dom
 create table insurer_insurance_type
 (
     insurer_id        int not null,
@@ -29,12 +32,14 @@ create table insurer_insurance_type
     foreign key (insurance_type_id) references insurance_type (id) on update cascade on delete restrict
 );
 
+-- hydraulik
 create table service
 (
     id   int auto_increment primary key,
     name varchar(250) not null
 );
 
+-- aviva + dom + hydraulik + dostępny 3 / 5 /... razy
 create table iit_service
 (
     id                 int auto_increment primary key,
@@ -48,6 +53,7 @@ create table iit_service
         references insurer_insurance_type (insurer_id, insurance_type_id) on update cascade on delete restrict
 );
 
+-- moje ubezpieczenie wykupione w listopadzie 2024 ważne przez rok
 create table my_insurance
 (
     id                 int auto_increment primary key,
@@ -60,6 +66,8 @@ create table my_insurance
         references insurer_insurance_type (insurer_id, insurance_type_id) on update cascade on delete restrict
 );
 
+-- hydraulik + grudzień 2024
+-- hydraulik + styczeń 2025
 create table service_tracking
 (
     id              int auto_increment primary key,
@@ -68,3 +76,11 @@ create table service_tracking
 
     foreign key (my_insurance_id) references my_insurance (id) on update cascade on delete restrict
 );
+
+
+-- zadanie domowe:
+-- 1. wypełnić tabele danymi oo ubezpieczeniach z ostatnich 3 lat w tym przynajmniej jedno aktualne
+-- 2. Napisać zapytanie, które zwróci mi listę aktualnie otwartych ubezpieczeń
+    -- otwarte ubezpieczenie w usługach ma serwis hydraulika dostępny przynajmniej 3 razy w trakcie trwania ubezpieczenia
+    -- skorzystałem z usług hydraulika już 2 razy
+-- 3. Napisać zapytanie, które zwróci mi informację o tym, ile razy mogę wezwać hydraulika
