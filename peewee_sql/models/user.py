171707501b3650ef0195a8517700b591ce5c41ff
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-02-08 12:07:08 +0000

    UserModel

---
+++ peewee_sql/models/user.py
@@ -0,0 +1,18 @@
+from peewee import *
+
+from peewee_sql.models.base_model import BaseModel
+from peewee_sql.models.first_name import FirstName
+from peewee_sql.models.last_name import LastName
+
+
+class User(BaseModel):
+    id = AutoField(column_name="user_id")
+    email = CharField(column_name="user_email", max_length=100, null=True)
+    phone = CharField(column_name="user_phone", max_length=12, null=True)
+    card_number = CharField(column_name="user_card_number", max_length=9, null=True)
+    first_name = ForeignKeyField(FirstName, null=False, column_name="user_first_name_id")
+    last_name = ForeignKeyField(LastName, null=False, column_name="user_last_name_id")
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
--- peewee_sql/pmain.py
+++ peewee_sql/pmain.py
@@ -1,8 +1,8 @@
 import logging
 
-from peewee_sql.models.author import Author
 from peewee_sql.models.first_name import FirstName
 from peewee_sql.models.last_name import LastName
+from peewee_sql.models.user import User
 
 
 def main() -> None:
@@ -10,18 +10,14 @@ def main() -> None:
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
 
-    authors = (
-        Author.select(Author, FirstName, LastName)
-        .join(FirstName, on=(Author.first_name == FirstName.id))
-        .join(LastName, on=(Author.last_name == LastName.id))
+    users = (
+        User.select(User, FirstName, LastName)
+        .join(FirstName, on=(User.first_name == FirstName.id))
+        .join(LastName, on=(User.last_name == LastName.id))
     )
 
-    Author.books.get_through_model()
-
-    for author in authors:
-        print(author)
-        for book in author.books:
-            print(f"    -> {book.get()}")
+    for user in users:
+        print(user)
 
 
 if __name__ == '__main__':
