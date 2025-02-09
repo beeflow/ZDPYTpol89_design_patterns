7a9d23607fa34449b1d519b1fb83c584cbb655b8
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-02-09 13:38:08 +0000

    Book rent

--- alchemy/main.py
+++ alchemy/main.py
@@ -1,38 +1,37 @@
-from typing import Optional
-
-from alchemy.models.author import Author
 from alchemy.models.book_copy import BookCopy
-from alchemy.models.first_name import FirstName
-from alchemy.models.last_name import LastName
 from alchemy.settings import session
 
 
 def main() -> None:
-    authors: list[Author] = session.query(Author).all()
-    for author in authors:
-        print(author)
+    # authors: list[Author] = session.query(Author).all()
+    # for author in authors:
+    #     print(author)
         # print(author.books[0] if len(author.books) != 0 else "")
 
     # books: list[Book] = session.query(Book).all()
     # for book in books:
     #     print(book.authors[0] if len(book.authors) != 0 else "")
 
-    first_name: Optional[FirstName] = session.query(FirstName).where(FirstName.name == "Jakiesimie").first()
-    last_name: Optional[LastName] = session.query(LastName).where(LastName.name == "Jakiesnazwisko").first()
-
-    if first_name is None:
-        first_name = FirstName(name="Jakiesimie")
-
-    if last_name is None:
-        last_name = LastName(name="Jakiesnazwisko")
-
-    print(first_name, last_name)
+    # first_name: Optional[FirstName] = session.query(FirstName).where(FirstName.name == "Jakiesimie").first()
+    # last_name: Optional[LastName] = session.query(LastName).where(LastName.name == "Jakiesnazwisko").first()
+    #
+    # if first_name is None:
+    #     first_name = FirstName(name="Jakiesimie")
+    #
+    # if last_name is None:
+    #     last_name = LastName(name="Jakiesnazwisko")
+    #
+    # print(first_name, last_name)
     # author = Author(first_name=first_name, last_name=last_name)
 
     bcs: list[BookCopy] = session.query(BookCopy).all()
-    for bc in bcs:
-        print(bc)
 
+    for bc in bcs:
+        print(bc.book_rents)
+
+    # for ub in session.query(UserBookRent).all():
+    #     print(ub.book_copy)
+    #
     # session.add(author)
     # session.commit()
 
---
+++ alchemy/models/user_book_rent.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Date, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+from alchemy.models.base_model import BaseModel
+from alchemy.models.book_copy import BookCopy
+from alchemy.models.user import User
+
+
+class UserBookRent(BaseModel):
+    __tablename__ = 'user_book_rent'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    book_copy_id = Column(Integer, ForeignKey('book_copy.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
+    rented_on = Column(Date, nullable=True)
+    returned_on = Column(Date, nullable=True)
+
+    book_copy = relationship(BookCopy, backref='book_rents')
+    user = relationship(User, backref='book_rents')
+
+    def __str__(self):
+        return f'{self.book_copy} {self.user}'
