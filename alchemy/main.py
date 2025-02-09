4eb8fd463719bac8e56fc397bea082809a0d80b8
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-02-09 10:36:42 +0000

    Book and manytomany

--- alchemy/main.py
+++ alchemy/main.py
@@ -1,17 +1,16 @@
+from alchemy.models.author import Author
 from alchemy.models.book import Book
 from alchemy.settings import session
 
 
 def main() -> None:
-    # authors: list[Author] = session.query(Author).all()
-    #
-    # for author in authors:
-    #     print(author)
+    authors: list[Author] = session.query(Author).all()
+    for author in authors:
+        print(author.books[0] if len(author.books) != 0 else "")
 
     books: list[Book] = session.query(Book).all()
-
     for book in books:
-        print(book)
+        print(book.authors[0] if len(book.authors) != 0 else "")
 
 
 if __name__ == '__main__':
